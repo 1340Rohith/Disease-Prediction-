e2d960f24398ea80207910705c4dd0eaca65239f
1340Rohith <rohith.dl009@gmail.com>
2024-09-10 21:07:55 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import train
 import pyautogui
 pyautogui.moveTo(100, 200)
 pyautogui.moveTo(200, 300)
+os.environ['DISPLAY'] = ':0'
 st.title("Disease Diagnosis")
 def refresh():
     pyautogui.hotkey("ctrl","F5")
